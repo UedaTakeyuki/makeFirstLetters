c8cf3e432ffd15c820ef26d54915886687f7bf96
Takeyuki UEDA <ueda@latelierdueda.com>
2023-03-04 14:24:26 +0900

    updated

--- arg.ts
+++ arg.ts
@@ -1,1 +1,12 @@
-console.log(Deno.args);
+console.log(Deno.args)
+
+import { parse } from "https://deno.land/std@0.175.0/flags/mod.ts";
+
+const flags = parse(Deno.args, {
+  boolean: ["help", "color"],
+  string: ["version"],
+  default: { color: true },
+});
+console.log("Wants help?", flags.help);
+console.log("Version:", flags.version);
+console.log("Wants color?:", flags.color);
--- main.ts
+++ main.ts
@@ -1,5 +1,21 @@
 //const file = await Deno.open("pokedex.json");
 //console.log(file)
-import pokemon_json from './pokedex.js'
-console.log(Array.from(new Set(pokemon_json.map(p => p.name.jpn[0]))).sort(function(x,y){return x.localeCompare(y, 'ja')}).map(p => [p,p]))
-console.log(JSON.stringify(Array.from(new Set(pokemon_json.map(p => p.name.cht[0]))).sort(function(x,y){return x.localeCompare(y, 'zh')}).map(p => [p,p]), null, 2))
+import pokedex from './pokedex.js'
+
+import { parse } from "https://deno.land/std@0.175.0/flags/mod.ts";
+
+const flags = parse(Deno.args, {
+  string: ["lang"],
+});
+
+switch (flags.lang) {
+  case "jpn":
+    console.log(Array.from(new Set(pokedex.map(p => p.name.jpn[0]))).sort(function(x,y){return x.localeCompare(y, 'ja')}).map(p => [p,p]))
+    break
+  case "cht":
+    console.log(JSON.stringify(Array.from(new Set(pokedex.map(p => p.name.cht[0]))).sort(function(x,y){return x.localeCompare(y, 'zh')}).map(p => [p,p]), null, 2))
+    break
+  case "fra":
+    console.log(JSON.stringify(Array.from(new Set(pokedex.map(p => p.name.fra[0]))).sort(function(x,y){return x.localeCompare(y, 'fr')}).map(p => [p,p])))
+    break
+}
